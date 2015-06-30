961eae4b54563102d47254fe4153b34804d3432b
imatvieiev <prodesigning@ya.ru>
2015-06-30 14:08:40 +0300

    id length bug fixed

--- tests/collisionTest.js
+++ tests/collisionTest.js
@@ -11,10 +11,8 @@ function findCollisions(iterations){
     return true;
 }
 
-/*describe('add', function(){
+describe('add', function(){
     it('there are no collisions', function(){
         assert.equal(findCollisions(200), true);
     });
-});*/
-
-console.log(id().length);
+});
